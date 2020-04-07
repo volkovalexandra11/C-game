59a8b785a5d147708d19130d4284122e7a250439
Lugovykh Sergei <46783403+aynug24@users.noreply.github.com>
2020-04-07 21:50:12 +0500

    Add files via upload
    BranchMade

--- Program.cs
+++ Program.cs
@@ -1,20 +1,59 @@
 ﻿using System;
-using System.Drawing;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace The_fucking_game
+namespace The_Game
 {
-    //public class GameForm: Form
-    
-    
-    
-    
-    class Program
+    public class Player
     {
-        static void Main(string[] args)
+        public int PlayerX { get; private set; }
+        public int PlayerY { get; private set; }
+        public static int Height = 100;
+
+        public GameState Game { get; }
+
+        public Player(GameState game)
         {
-            Console.WriteLine("Hello World!");
-            var form = new Form();
+            Game = game;
+        }
+
+
+    }
+
+    public class GameState
+    {
+        public Player Player { get; }
+        public Size LevelSize { get; private set; }
+
+        public GameState(int lvlWidth, int lvlHeight)
+        {
+            Player = new Player(this);
+            LevelSize = new Size(lvlWidth, lvlHeight);
+        }
+    }
+
+    public class TestForm : Form
+    {
+        public TestForm(GameState gs)
+        {
+            KeyDown +=
+                }
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+       // [STAThread] ?
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TestForm());
         }
     }
 }
