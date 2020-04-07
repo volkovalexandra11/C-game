70673b5b47bd4c067798ab1ec5a197de2081dad7
volkovalexandra11 <57279815+volkovalexandra11@users.noreply.github.com>
2020-04-07 21:47:52 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,59 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Collections.Generic;
 
-namespace The_Game
+namespace The_fucking_game
 {
-    public class Player
+    //public class GameForm: Form
+    
+    
+    
+    
+    class Program
     {
-        public int PlayerX { get; private set; }
-        public int PlayerY { get; private set; }
-        public static int Height = 100;
-
-        public GameState Game { get; }
-
-        public Player(GameState game)
+        static void Main(string[] args)
         {
-            Game = game;
-        }
-
-
-    }
-
-    public class GameState
-    {
-        public Player Player { get; }
-        public Size LevelSize { get; private set; }
-
-        public GameState(int lvlWidth, int lvlHeight)
-        {
-            Player = new Player(this);
-            LevelSize = new Size(lvlWidth, lvlHeight);
-        }
-    }
-
-    public class TestForm : Form
-    {
-        public TestForm(GameState gs)
-        {
-            KeyDown +=
-                }
-    }
-
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-       // [STAThread] ?
-        static void Main()
-        {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TestForm());
+            Console.WriteLine("Hello World!");
+            var form = new Form();
         }
     }
 }
