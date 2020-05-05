2acb9855982123db02ff08679b0454a1f4673070
Alexandra Volkova <57279815+volkovalexandra11@users.noreply.github.com>
2020-05-06 04:20:33 +0500

    New tests appeared and went to their own folder

---
+++ Tests/GameStateTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using The_Game.Levels;
+using The_Game.Mobs;
+using The_Game.Model;
+
+namespace The_Game.Tests
+{
+    class GameStateTests
+    {
+        private static readonly GameState Gs = new GameState();
+        private readonly Player player = new Player(Gs);
+        
+        [Test]
+        public void GameStateLevelTest()
+        {
+            var levelType = new Level(Gs, new Level12ForTests(), player, nameof(Level12ForTests)).LevelName;
+            var thisType = Gs.Level.LevelName;
+            Assert.True(thisType == levelType);
+        }
+
+        [Test]
+        public void GameStateEntitiesTest()
+        {
+            var crumbledWall = new CrumbledWall(new Size(150, 400), new Point(100, 300 + 10));
+            var stump = new Stump(Gs, new Size(100, 80), new Point(650, 620 + 10));
+            Assert.True(Gs.Level.Entities.Contains(crumbledWall));
+            Assert.True(!Gs.Level.Entities.Contains(stump));
+        }
+
+        [Test]
+        public void GameStateChangesLevel()
+        {
+            var level = Gs.Level;
+            var newLevel = new LevelForTestsWithGraph();
+            Gs.ChangeLevel(newLevel);
+            Assert.AreNotEqual(level, Gs.Level);
+        }
+    }
+}
---
+++ Tests/PlayerActionsTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using NUnit.Framework;
+using The_Game.Levels;
+using The_Game.Mobs;
+using The_Game.Model;
+
+namespace The_Game
+{
+    public class PlayerActionsTests
+    {
+        private static readonly GameState Gs = new GameState();
+        private static Player player = new Player(Gs);
+        private Vector2 initialPosition;
+
+        [Test]
+        public void TestGoingRightHorizontalPos()
+        {
+            Gs.PlayerActions.Add(MobAction.GoRight);
+            player.Update();
+            Assert.True(initialPosition.X < player.Pos.X);
+        }
+
+        [Test]
+        public void TestGoingRightVerticalPos()
+        {
+            Gs.PlayerActions.Add(MobAction.GoRight);
+            player.Update();
+            Assert.AreEqual(initialPosition.Y, player.Pos.Y, 1e-7);
+        }
+
+        [Test]
+        public void TestGoingLeftHorizontalPos()
+        {
+            Gs.PlayerActions.Add(MobAction.GoLeft);
+            player.Update();
+            Assert.True(initialPosition.X > player.Pos.X);
+            Assert.AreEqual(initialPosition.Y, player.Pos.Y, 1e-7);
+        }
+
+        [Test]
+        public void TestGoingLeftVerticalPos()
+        {
+            Gs.PlayerActions.Add(MobAction.GoLeft);
+            player.Update();
+            Assert.AreEqual(initialPosition.Y, player.Pos.Y, 1e-7);
+        }
+
+
+        [Test]
+        public void TestJumping()
+        {
+            Gs.PlayerActions.Add(MobAction.Jump);
+            player.Update();
+            Assert.AreEqual(initialPosition.X, player.Pos.X, 1e-7);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            var level = new Level12ForTests();
+            Gs.ChangeLevel(level);
+            player = new Player(Gs);
+            var plLevel = new Level(Gs, new Level12ForTests(), player, nameof(Level12ForTests));
+            player.ChangeLevel(plLevel);
+            Gs.PlayerActions.Clear();
+            initialPosition = player.Pos;
+        }
+    }
+}
