642b730337bea4cbea5e232491d5c2a9c2462745
Alexandra Volkova <57279815+volkovalexandra11@users.noreply.github.com>
2020-05-05 23:08:38 +0500

    Tests

--- Tests.cs
+++ Tests.cs
@@ -1,35 +1,69 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using NUnit.Framework;
+﻿using System.Drawing;
+using System.Numerics;
+using NUnit.Framework;
+using The_Game.Levels;
+using The_Game.Mobs;
+using The_Game.Model;
 
-//namespace The_Game
-//{
-//    //[TestCase]
-//    public class Tests
-//    {
-//        [Test]
-//        public void TestGoing()
-//        {
-//            var gs = new GameState();
-//            var player = new Player(gs);
-//            player.UpdatePosition();
-//            var pos = player.Pos + 10 * player.Velocity;
-//            player.State = PlayerState.Walking;
-//            Assert.AreEqual(pos, player.Pos);
-//        }
+namespace The_Game
+{
+    public class Tests
+    {
+        private static readonly GameState Gs = new GameState();
+        private static Player player = new Player(Gs);
+        private Vector2 initialPosition;
 
-//        [Test]
-//        public void TestJumping()
-//        {
-//            var gs = new GameState();
-//            var player = new Player(gs);
-//            player.UpdatePosition();
-//            var pos = player.Pos + 10 * player.Velocity;
-//            player.State = PlayerState.Jumping;
-//            Assert.AreEqual(pos, player.Pos);
-//        }
-//    }
-//}
+        [Test]
+        public void TestGoingRight()
+        {
+            Gs.PlayerActions.Add(MobAction.GoRight);
+            player.Update();
+            Assert.True(initialPosition.X < player.Pos.X);
+            Assert.AreEqual(initialPosition.Y, player.Pos.Y, 1e-7);
+        }
+
+        [Test]
+        public void TestGoingLeft()
+        {
+            Gs.PlayerActions.Add(MobAction.GoLeft);
+            player.Update();
+            Assert.True(initialPosition.X > player.Pos.X);
+            Assert.AreEqual(initialPosition.Y, player.Pos.Y, 1e-7);
+        }
+
+        [Test]
+        public void TestJumping()
+        {
+            Gs.PlayerActions.Add(MobAction.Jump);
+            player.Update();
+            Assert.AreEqual(initialPosition.X, player.Pos.X, 1e-7);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            player = new Player(Gs);
+            var level = new Level12ForTests();
+            Gs.ChangeLevel(level);
+            Gs.PlayerActions.Clear();
+            initialPosition = player.Pos;
+        }
+
+        [Test]
+        public void GameStateLevelTest()
+        {
+            var levelType = "Level 12 For Tests";
+            var thisType = Gs.Level.LevelName;
+            Assert.True(thisType == levelType);
+        }
+
+        [Test]
+        public void GameStateEntitiesTest()
+        {
+            var crumbledWall = new CrumbledWall(new Size(150, 400), new Point(100, 300 + 10));
+            var stump = new Stump(Gs, new Size(100, 80), new Point(650, 620 + 10));
+            Assert.True(Gs.Level.Entities.Contains(crumbledWall));
+            Assert.True(!Gs.Level.Entities.Contains(stump));
+        }
+    }
+}
