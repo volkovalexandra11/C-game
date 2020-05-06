b2c377ee4fa79d6bb1a045a133dc8af0d1a4cdce
Alexandra Volkova <57279815+volkovalexandra11@users.noreply.github.com>
2020-05-06 13:08:03 +0500

    Level For Mob Tests fixed

--- Levels/LevelForTestsWithGraph.cs
+++ Levels/LevelForTestsWithGraph.cs
@@ -16,16 +16,27 @@ namespace The_Game.Levels
         public LevelData BuildData(GameState game, Level level)
         {
             var size = new Size(1800, 1000);
-            var waypoints = new[]
+            var waypoints = new Vector2[]
             {
-                new Vector2(0, 0), new Vector2(1, 2), new Vector2(2, 3), new Vector2(4, 3), new Vector2(5, 8)
+                new Vector2(1800, 300), new Vector2(1500, 300), new Vector2(1200, 300), new Vector2(900, 300),
+                new Vector2(750, 550), new Vector2(1050, 550),
+                new Vector2(200, 800), new Vector2(550, 800), new Vector2(900, 800), new Vector2(1200, 800),
+                new Vector2(1500, 800), new Vector2(1800, 800)
             };
-            var graphBlank = new[]
+            var adjacencyLists = new Vector2[][]
             {
-                new [] {new Vector2(0, 0), new Vector2(1, 2), new Vector2(2, 3), new Vector2(5, 8)},
-                new [] {new Vector2(1,2), new Vector2(2,3)},
-                new [] {new Vector2(2,3), new Vector2(4,3), new Vector2(5,8)},
-                new []{new Vector2(4,3), new Vector2(5,8)}
+                new [] { new Vector2(1800, 300), new Vector2(1500, 300) },
+                new [] { new Vector2(1500, 300), new Vector2(1800, 300), new Vector2(1200, 300) },
+                new [] { new Vector2(1200, 300), new Vector2(1500, 300), new Vector2(900, 300) },
+                new [] { new Vector2(900, 300), new Vector2(1200, 300), new Vector2(750, 550), new Vector2(1050, 550) },
+                new [] { new Vector2(750, 550), new Vector2(550, 800), new Vector2(900, 800) },
+                new [] { new Vector2(1050, 550), new Vector2(900, 800), new Vector2(1200, 800)},
+                new [] { new Vector2(200, 800), new Vector2(550, 800) },
+                new [] { new Vector2(550, 800), new Vector2(200, 800), new Vector2(900, 800) },
+                new [] { new Vector2(900, 800), new Vector2(550, 800), new Vector2(1200, 800) },
+                new [] { new Vector2(1200, 800), new Vector2(900, 800), new Vector2(1500, 800) },
+                new [] { new Vector2(1500, 800), new Vector2(1200, 800), new Vector2(1800, 800) },
+                new [] { new Vector2(1800, 800), new Vector2(1500, 800) }
             };
             return new LevelData
             (
@@ -36,11 +47,11 @@ namespace The_Game.Levels
                     new Background(size),
                     new Ground(new Size(1800, 300), new Point(0, 700)),
                     new CrumbledWall(new Size(150, 400), new Point(100, 300 + 10)),
-                    new Ladder(new Size(100, 80), new Point(650, 620 + 10)),
-                    new Rogue(game, level, new Vector2(5, 8))
+                    //new Ladder(new Size(100, 80), new Point(650, 620 + 10)),
+                    new Rogue(game, level, new Vector2(1200, 8))
                 }.AsReadOnly(),
                 waypoints,
-                LevelData.GetReverseGraph(graphBlank)
+                LevelData.GetReverseGraph(adjacencyLists)
             );
         }
     }
